48388e10bc68c52937cf6117f32ce9487fca7974
nikhil-dev-395 <nikhilw395@gmail.com>
2024-11-26 17:17:33 +0530

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,ejs}", // Adjust this path to where your files are
+    "./public/**/*.{html,js,jsx,ts,tsx,ejs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
